30c88f9b53f57fa9f7a7d7dbf2923a39803f0909
Gollim <virtualhowl@gmail.com>
2018-11-17 16:23:47 +0100

    faith 2

--- NinJump/Motor2D/j1Entity_Manager.h
+++ NinJump/Motor2D/j1Entity_Manager.h
@@ -44,7 +44,7 @@ public:
 	bool CleanUp();
 	bool Load(pugi::xml_node& node);
 	bool Save(pugi::xml_node& node) const;
-	bool restart = false;
+	bool restart = false; //Faith
 private: 
 	p2List<j1Entity*> entities; 
 	float accumulated_time = 0.0f; 
